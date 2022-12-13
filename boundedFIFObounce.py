df44fa594f2424b52c5eb6401a74e994c4c94613
DevPapel <cool.jef77@gmail.com>
2022-12-13 15:21:52 +0800

    Added queue list

--- boundedFIFObounce.py
+++ boundedFIFObounce.py
@@ -4,12 +4,6 @@ bounceSize = 3
 
 que = input("Enter your name to line up the queue: ")
 
-FIFO = Queue("1", "2")
-FIFO.enqueue("3")
-FIFO.enqueue("4")
-print("Queue Count:",len(FIFO))
-
-for i in FIFO:
-    print("Current in line:",i)
-
-print("Updated Queue Count:", len(FIFO))
+queList = []
+if len(queList) < 3:
+    print("test")
