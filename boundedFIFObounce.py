1bacb08a673578f7ed3faf68a5f3c82a809ca217
DevPapel <cool.jef77@gmail.com>
2022-12-13 15:14:02 +0800

    Added bounceSize and user input to the FIFO file

---
+++ boundedFIFObounce.py
@@ -0,0 +1,15 @@
+from queueclass import Queue
+
+bounceSize = 3
+
+que = input("Enter your name to line up the queue: ")
+
+FIFO = Queue("1", "2")
+FIFO.enqueue("3")
+FIFO.enqueue("4")
+print("Queue Count:",len(FIFO))
+
+for i in FIFO:
+    print("Current in line:",i)
+
+print("Updated Queue Count:", len(FIFO))
