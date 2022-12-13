16f4a985779685353e2175dbdf37996abd963832
DevPapel <cool.jef77@gmail.com>
2022-12-13 16:25:16 +0800

    Implementation of messages

--- PrioQue.py
+++ PrioQue.py
@@ -7,3 +7,14 @@ Very_Important = 3
 Important = 2
 Neutral = 1
 # End of Priority Values #
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(Important, "Windshield wipers turned on")
+messages.enqueue_with_priority(Very_Important, "Radio station tuned in")
+messages.enqueue_with_priority(Essential, "Brake pedal depressed")
+messages.enqueue_with_priority(Critical, "Hazard lights turned on")
+
+print(messages.dequeue())
+print(messages.dequeue())
+print(messages.dequeue())
+print(messages.dequeue())
